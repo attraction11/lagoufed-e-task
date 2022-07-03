6ead7f2a408d802bb950774a5ea262a5e85654b2
LAPTOP-NDTF2MRT\jiank <cjk7072@closeli.cn>
2022-07-04 07:23:49 +0800

    feat: del

--- part4/fed-e-task-04-04/notes/4-4-1/资料/react-ssr/src/server/http.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.use(express.static('public'));
-
-app.listen(3000, () => console.log("server is listening on 3000 port"));
-
-export default app;
